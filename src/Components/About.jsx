7d4e8c95117f9b3fda40a0fd0eb8e04802fb6cfe
akhil3838 <akhiltv713@gmail.com>
2024-07-30 23:03:17 +0530

    name add

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,6 +6,9 @@ function About() {
   return (
     <>
       <p>About</p>
+      <p>akhil</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In non placeat, tenetur asperiores animi eaque officiis iusto, at odio incidunt facere dolore? Soluta quo provident, repellat quibusdam veritatis dolor ut!</p>
+      <button>sumb</button>
     </>
   )
 }
